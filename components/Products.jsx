4b6882eca626c8ba6bda4032aad85bbd64f6b8a9
Parna Das <dparna28@gmail.com>
2026-06-19 19:11:03 +0530

    all commit

--- components/Products.jsx
+++ components/Products.jsx
@@ -1,36 +1,35 @@
 import React from "react"
-import useFetch from "../hooks/useFetch"
+import useFetch from "../hooks/useFetch.js";
 
 const Products = () => {
-    const { data, loading, error } = useFetch(
-        "https://api.escuelajs.co/api/v1/products"
-    );
-
+    const {data,loading,error} = useFetch("https://api.escuelajs.co/api/v1/products");
+    
     return (
-        <div className="min-h-screen bg-black text-white px-6 py-12">
+        <div className="min-h-screen bg-blak text-white px-6 py-12">
             {loading && (
-                <div className="text-center text-3xl mt-60">Loading...</div>
+                <div className="text-center text-3xl mt-60">Loading....</div>
             )}
-
+            
             {error && (
-                <div className="text-center text-red-500 text-3xl mt-60">Error: {error}</div>
+                <div className="text-center text-red-500 text-3xl mt-60">Error:{error}</div>
             )}
+            
             {!loading && !error && (
                 <>
-                    <h1 className="text-3xl font-bold text-center">Photos</h1>
+                    <h1 className="text-3xl text-center font-bold">Photos</h1>
                     <div className="grid grid-cols-4 gap-6 mt-10">
-                        {data &&
-                            data.slice(0, 72).map((product) => (
-                                <div key={product.id} className="text-center">
-                                    <div className="border-2 border-white px-12 py-6 mb-4">
-                                        <img src={product.images[0]}
-                                            alt={product.title}
-                                            className="w-full h-60 object-cover"
-                                        />
-                                        <h4 className="text-nowrap overflow-hidden">{product.title}</h4>
-                                    </div>
+                        {data && data.slice(0, 75).map((product) => {
+                            <div key={product.id} className="text-center">
+                                <div className="border-2 border-white px-12 py-6 mb-4">
+                                    <img
+                                    src={product.images[0]}
+                                    alt={product.title}
+                                    className="w-full h-60 object-cover"
+                                     />
+                                    <h4 className="text-nowrap overflow-hidden">{product.title}</h4>
                                 </div>
-                            ))}
+                            </div>
+                        })}
                     </div>
                 </>
             )}
--- hooks/useFetch.js
+++ hooks/useFetch.js
@@ -1,32 +1,31 @@
-import { useState,useEffect,useCallback } from "react"
+import {useCallback, useEffect, useState} from "react"
 
 const useFetch = (url) => {
-    const [data, setData] = useState(null)
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState(null)
-
+    const[data, setData] = useState(null);
+    const[loading, setLoading] = useState(true);
+    const[error, setError] = useState(null);
+    
     const fetchData = useCallback(async () => {
-        try {
-            setError(null)
-            const response = await fetch(url)
-            if (!response.ok) {
-                throw new Error("Failed to Fetch data")
+        try{
+            setError(false);
+            const response = await fetch(url);
+            if(!response.ok) {
+                throw new error("Failed to fetch data");
             }
-            const result = await response.json()
-            setData(result)
+            const result = await response.json();
+            setData(result); 
         }catch(err){
-            setError(err.message || "Something went wrong")
+            setError(err.message || "Something went wrong");
         }finally{
-            setLoading(false)
+            setLoading(false);
         }
     }, [url])
-
-    useEffect(()=>{
-        fetchData()
-    }, [fetchData]);
-
+    
+    useEffect(() => {
+        fetchData();
+    }, [fetchData])
+    
     return {data, loading, error};
-};
+}
 
 export default useFetch;
-
